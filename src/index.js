ba128bb563e8dd747e0291892a42f53c4f68aa1e
nickprudnik <prudniknick@gmail.com>
2018-03-11 22:28:26 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function longestConsecutiveLength(array) {
+  if (array.length == 0) {
+        return 0;
+    }
+
+    var nick = [];
+    var max = 1;
+
+    for (var i=0; i<array.length; i++)
+        nick.push(array[i]);
+
+    for (var i=0; i<array.length; i++) {
+
+        var right = array[i] + 1;
+        var count = 1;
+
+        while (nick.indexOf(right) != -1) {
+            count++;
+            nick.splice(nick.indexOf(right), 1);
+            right++;
+        }
+
+        max = Math.max(count, max);
+    }
+
+    return max;
+
+
   // your solution here
 }
